6f48a74b28aa5e492fa6d78d6816b26e087960fc
Brice Videau <bvideau@anl.gov>
2021-11-23 15:55:40 -0600

    Minor fixes and improvement to CCS support.

--- skopt/optimizer/optimizer.py
+++ skopt/optimizer/optimizer.py
@@ -9,7 +9,6 @@ try:
     import cconfigspace as CCS
     ccs_active = True
 except ImportError as a:
-    import warnings
     warnings.warn("CCS could not be loaded and is deactivated: " + str(a), category=ImportWarning)
 
 import numpy as np
